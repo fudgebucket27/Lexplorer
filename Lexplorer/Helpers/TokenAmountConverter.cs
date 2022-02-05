925414d261f111b342b4bc04945db61544f8bed6
John <fudgebucket27@gmail.com>
2022-02-06 08:43:50 +1000

    fee amounts and amount working for swap and transfer

---
+++ Lexplorer/Helpers/TokenAmountConverter.cs
@@ -0,0 +1,11 @@
+﻿namespace Lexplorer.Helpers
+{
+    public static class TokenAmountConverter
+    {
+        public static string Convert(string balance, int decimals)
+        {
+            var result = Double.Parse(balance) / Math.Pow(10, (double)decimals);
+            return result.ToString();
+        }
+    }
+}
