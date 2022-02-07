af1a2f0113f1e05d3da64f5fec3303bf55a01ace
John <john.trembath@glencore.com.au>
2022-02-07 16:43:56 +1000

    previous page navigation from swap page

--- Lexplorer/Helpers/ParameterHelper.cs
+++ Lexplorer/Helpers/ParameterHelper.cs
@@ -2,12 +2,12 @@
 {
     public static class ParameterHelper
     {
-        public static string ConvertToTransactionLink(string transactionType, string transactionId)
+        public static string ConvertToTransactionLink(string transactionType, string transactionId, string? previousPageNumber = "0")
         {
             switch (transactionType)
             {
                 case "Swap":
-                    return $"transaction/Swap/{transactionId}";
+                    return $"transaction/Swap/{transactionId}?previousPageNumber={previousPageNumber}";
                 default:
                     return "404";
             }
