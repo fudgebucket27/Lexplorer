24956cf8e149806b958174c2c17b4d08b4da76f6
Sebastian Modersohn <im24@msohn.de>
2022-03-02 12:55:18 +0100

    First milestone of CSV default format export finished * no AMM and NFT transactions yet - only generic id, timestamp etc. * even for the transactions already implemented, only the cases   I could personally verify with my wallet app implemented

--- Shared/Services/TranscationExportDefaultCSVFormat.cs
+++ Shared/Services/TranscationExportDefaultCSVFormat.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Lexplorer.Helpers;
 
 namespace Lexplorer.Services
 {
@@ -11,13 +12,122 @@ namespace Lexplorer.Services
         public void WriteHeader(CSVWriteLine writeLine)
         {
             sb.Clear();
-            sb.AppendJoin(Convert.ToChar(9), "Tx-ID", "Timestamp", "Type");
+            sb.AppendJoin(Convert.ToChar(9), "Tx-ID", "Timestamp", "Type", "From", "To", "Added", "Added Token", "Fee", "Fee Token", "Total", "Total Token");
             writeLine(sb.ToString());
+        }
+
+        private string? GetExportAmount(double amount, Token? token)
+        {
+            return TokenAmountConverter.ToString(amount, token?.decimals ?? 1, 1, "");
+        }
+
+        //helper with default param values for more readable code
+        private void DoBuildLine(string? id, string? timestamp, string? type, string? from = null, string? to = null, 
+            string? added = null, string? addedToken = null, string? fee = null, string? feeToken = null, string? total = null, string? totalToken = null)
+        {
+            sb.AppendJoin(Convert.ToChar(9), id, timestamp, type, from, to, added, addedToken, fee, feeToken, total, totalToken);
+        }
+        private void WriteOrderBookTrade(OrderBookTrade orderBookTrade, string accountIdPerspective, CSVWriteLine writeLine)
+        {
+            bool areWeAccountA = orderBookTrade.accountA?.id == accountIdPerspective;
+            bool buyOrSellForUs = areWeAccountA ? orderBookTrade.fillAmountBorSA : orderBookTrade.fillAmountBorSB;
+            if (areWeAccountA)
+            {
+                if (orderBookTrade.fillAmountBorSA)
+                {
+                    DoBuildLine(orderBookTrade.id, orderBookTrade.verifiedAt, orderBookTrade.typeName);
+                }
+                else
+                {
+                    DoBuildLine(orderBookTrade.id, orderBookTrade.verifiedAt, orderBookTrade.typeName,
+                        from: orderBookTrade.accountA?.address, to: orderBookTrade.accountB?.address,
+                        added: GetExportAmount(orderBookTrade.fillBA - orderBookTrade.feeA, orderBookTrade.tokenB), //Amount of token B bought by Account A
+                        addedToken: orderBookTrade.tokenB?.symbol,
+                        fee: GetExportAmount(orderBookTrade.feeA, orderBookTrade.tokenB), //Fee paid by Account A with Token B
+                        feeToken: orderBookTrade.tokenB?.symbol,
+                        total: GetExportAmount(orderBookTrade.fillSA, orderBookTrade.tokenA), //Amount of token A sold by Account A
+                        totalToken: orderBookTrade.tokenA?.symbol
+                        );
+                }
+            }
+            else
+            {
+                if (orderBookTrade.fillAmountBorSA)
+                {
+                    DoBuildLine(orderBookTrade.id, orderBookTrade.verifiedAt, orderBookTrade.typeName);
+                }
+                else
+                {
+                    DoBuildLine(orderBookTrade.id, orderBookTrade.verifiedAt, orderBookTrade.typeName,
+                        from: orderBookTrade.accountA?.address, to: orderBookTrade.accountB?.address,
+                        added: GetExportAmount(orderBookTrade.fillBB, orderBookTrade.tokenA), //Amount of token A bought by Account B
+                        addedToken: orderBookTrade.tokenA?.symbol,
+                        fee: GetExportAmount(orderBookTrade.feeA, orderBookTrade.tokenB), //doc wrong: Fee paid by Account A with Token B
+                        feeToken: orderBookTrade.tokenB?.symbol,
+                        total: GetExportAmount(orderBookTrade.fillSB, orderBookTrade.tokenB), //Amount of token B sold by Account B
+                        totalToken: orderBookTrade.tokenB?.symbol
+                        );
+                }
+            }
+        }
+        private void WriteSwap(Swap swap, string accountIdPerspective, CSVWriteLine writeLine)
+        {
+            if (swap.fillAmountBorSA)
+            {
+                //a buy for us, 
+                DoBuildLine(swap.id, swap.verifiedAt, swap.typeName,
+                    from: swap.account?.address,
+                    to: swap.pool?.address
+
+
+                    );
+            }
+            else
+            {
+                //we are account A and bought token B, selling token A, paying feeA in tokenB
+                DoBuildLine(swap.id, swap.verifiedAt, swap.typeName,
+                    from: swap.account?.address,
+                    to: swap.pool?.address,
+                    added: GetExportAmount(swap.fillBA - swap.feeA, swap.tokenB), //Amount of token B bought by Account A minus feeA we paid
+                    addedToken: swap.tokenB?.symbol,
+                    fee: GetExportAmount(swap.feeA, swap.tokenB),  //Fee paid by Account A with Token B
+                    feeToken: swap.tokenB?.symbol,
+                    total: GetExportAmount(swap.fillSA, swap.tokenA), //Amount of token A sold by Account A
+                    totalToken: swap.tokenA?.symbol
+                    );
+            }
+        }
+        private void WriteTransfer(Transfer transfer, string accountIdPerspective, CSVWriteLine writeLine)
+        {
+            DoBuildLine(transfer.id, transfer.verifiedAt, transfer.typeName,
+                from: transfer.fromAccount?.address,
+                to: transfer.toAccount?.address,
+                added: TokenAmountConverter.ToString(transfer.amount, transfer.token?.decimals ?? 1, 1, ""),
+                addedToken: transfer.token?.symbol,
+                fee: TokenAmountConverter.ToString(transfer.fee, transfer.feeToken?.decimals ?? 1, 1, ""),
+                feeToken: transfer.feeToken?.symbol);
+        }
+        private void WriteAccountUpdate(AccountUpdate accountUpdate, string accountIdPerspective, CSVWriteLine writeLine)
+        {
+            DoBuildLine(accountUpdate.id, accountUpdate.verifiedAt, accountUpdate.typeName, 
+                from: accountUpdate.user?.address, 
+                fee: TokenAmountConverter.ToString(accountUpdate.fee, accountUpdate.feeToken?.decimals ?? 1, 1, ""),
+                feeToken: accountUpdate.feeToken?.symbol);
         }
         public void WriteTransaction(Transaction transaction, string accountIdPerspective, CSVWriteLine writeLine)
         {
             sb.Clear();
-            sb.AppendJoin(Convert.ToChar(9), transaction.id, transaction.verifiedAt, transaction.typeName);
+            if (transaction is OrderBookTrade)
+                WriteOrderBookTrade((OrderBookTrade)transaction, accountIdPerspective, writeLine);
+            else if (transaction is Transfer)
+                WriteTransfer((Transfer)transaction, accountIdPerspective, writeLine);
+            else if (transaction is Swap)
+                WriteSwap((Swap)transaction, accountIdPerspective, writeLine);
+            else if (transaction is AccountUpdate)
+                WriteAccountUpdate((AccountUpdate)transaction, accountIdPerspective, writeLine);
+            else
+                sb.AppendJoin(Convert.ToChar(9), transaction.id, transaction.verifiedAt, transaction.typeName);
+
             writeLine(sb.ToString());
         }
     }
---
+++ xUnitTests/LoopringGraphTests/TestAccountTransactionExportDefaultFormat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Lexplorer.Helpers;
+using Lexplorer.Models;
+using Lexplorer.Services;
+using JsonSubTypes;
+using Newtonsoft.Json;
+using xUnitTests.Utils;
+using Newtonsoft.Json.Linq;
+
+namespace xUnitTests.LoopringGraphTests
+{
+    [Collection("LoopringGraphQL collection")]
+    public class TestAccountTransactionExportDefaultFormat
+    {
+        GraphQLTestsFixture fixture;
+        LoopringGraphQLService service;
+        TranscationExportDefaultCSVFormat exporter;
+
+        public TestAccountTransactionExportDefaultFormat(GraphQLTestsFixture fixture)
+        {
+            this.fixture = fixture;
+            this.service = fixture!.LGS;
+            exporter = new TranscationExportDefaultCSVFormat();
+        }
+
+        [Theory]
+        [JsonFileData("AccountTransactions.json")]
+        public void TestExport(JArray transactionsJArray)
+        {
+            IList<Transaction> transactions = transactionsJArray.ToObject<IList<Transaction>>()!;
+            foreach (Transaction transaction in transactions)
+            {
+                exporter.WriteTransaction(transaction, fixture.testAccountId, (string line) =>
+                    {
+                        Assert.NotEmpty(line);
+                        //todo: test the line, split the elements
+                        //currently this test is only useful for debugging
+                    });
+            }
+        }
+    }
+}
