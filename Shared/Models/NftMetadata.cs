ef0c87b5c077ce7c2cc15ad9f22f192b71bcca73
Fudgey <fudgebucket27@gmail.com>
2022-03-19 16:35:44 +1000

    first version of nft view

---
+++ Shared/Models/NftMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Models
+{
+    public class NftMetadata
+    {
+        public string description { get; set;}
+        public string image { get; set; }
+        public string name { get; set; }
+        public int royalty_percentage { get; set; }
+    }
+}
---
+++ Shared/Services/NftMetadataService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Diagnostics;
+using Shared.Models;
+
+namespace Lexplorer.Services
+{
+    public class NftMetadataService : IDisposable
+    {
+        const string BaseUrl = "https://fudgey.mypinata.cloud/ipfs/";
+
+        readonly RestClient _client;
+
+        public NftMetadataService()
+        {
+            _client = new RestClient(BaseUrl);
+        }
+
+        public async Task<NftMetadata> GetMetadata(string link)
+        {
+            var request = new RestRequest(link);
+            try
+            {
+                var response = await _client.GetAsync(request);
+                return JsonConvert.DeserializeObject<NftMetadata>(response.Content);
+            }
+            catch (System.Net.Sockets.SocketException sex)
+            {
+                Trace.WriteLine(sex.StackTrace + "\n" + sex.Message);
+            }
+            catch (JsonReaderException e)
+            {
+                Trace.WriteLine(e.StackTrace + "\n" + e.Message);
+            }
+            return null;
+            ;
+        }
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
