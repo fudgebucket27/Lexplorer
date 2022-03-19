876915fbd637b3db01636584dc7ee91b7d16f0b6
Sebastian Modersohn <im24@msohn.de>
2022-03-19 17:13:44 +0100

    Added new NFTMetaData tests
    * just two hardcoded NFTs for now

---
+++ xUnitTests/NFTMetaDataTests/BaseNMDTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Lexplorer.Services;
+using Xunit;
+
+namespace xUnitTests.NFTMetaDataTests
+{
+    //shared context for several xUnit test classes
+    //https://xunit.net/docs/shared-context
+    public class NFTMetaDataTestsFixture : IDisposable
+    {
+        public NftMetadataService NMS { get; private set; }
+        public EthereumService EthS { get; private set; }
+
+        public NFTMetaDataTestsFixture()
+        {
+            NMS = new NftMetadataService();
+            EthS = new EthereumService();
+        }
+
+        public void Dispose()
+        {
+            NMS.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+
+    [CollectionDefinition("NFTMetaDataTests collection")]
+    public class LoopringGraphQLCollection : ICollectionFixture<NFTMetaDataTestsFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
+
---
+++ xUnitTests/NFTMetaDataTests/TestNFTMetaData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+using Lexplorer.Services;
+
+namespace xUnitTests.NFTMetaDataTests
+{
+	[Collection("NFTMetaDataTests collection")]
+	public class TestNFTMetaData
+	{
+		readonly NFTMetaDataTestsFixture fixture;
+
+		public TestNFTMetaData(NFTMetaDataTestsFixture fixture)
+		{
+			this.fixture = fixture;
+		}
+
+		[Theory]
+		[InlineData("0x4baf35a6982a81402fbe5882a47a75add97a01cc69fc418b5fc545026751f08a")]
+		[InlineData("0x78cc3ebffd8628722aaf29681b45d6a342e4ae11520c1d507894cc0c86049075")]
+		//[InlineData("0x01346618000000000000000002386f26fc10000000000000000000000000028d")] //loophead #653
+		public async void TestGetMetadata(string nftID)
+        {
+			var link = await fixture.EthS.GetMetadataLink(nftID);
+			Assert.NotNull(link);
+
+			var meta = await fixture.NMS.GetMetadata(link!);
+			Assert.NotNull(meta);
+        }
+	}
+}
+
