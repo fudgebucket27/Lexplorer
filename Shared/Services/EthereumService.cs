df7ee3b234f6b2cd2e9d0025e0f43674c7c9c799
Fudgey <fudgebucket27@gmail.com>
2022-03-20 12:17:43 +1000

    added new code for fall back to erc1155, erc721, then counterfactual nft. loopheads now display

--- Shared/Services/EthereumService.cs
+++ Shared/Services/EthereumService.cs
@@ -7,18 +7,29 @@ namespace Lexplorer.Services
 {
     public class EthereumService
     {
-        public async Task<string?> GetMetadataLink(string? tokenId)
+        public async Task<string?> GetMetadataLink(string? tokenId, string? tokenAddress)
         {
             if (tokenId == null) return null;
+            String? metadataLink = metadataLink = await GetMetadataLink(tokenId, tokenAddress, "function uri(uint256 id) external view returns (string memory)", "uri"); //call erc1155 contract 
+            if (metadataLink == null)
+                metadataLink = await GetMetadataLink(tokenId, tokenAddress, "function tokenURI(uint256 tokenId) public view virtual override returns (string memory)", "tokenURI"); //call erc721 nft contract 
+            if (metadataLink == null)
+                metadataLink = await GetMetadataLink(tokenId, "0xB25f6D711aEbf954fb0265A3b29F7b9Beba7E55d", "function uri(uint256 id) external view returns (string memory)", "uri"); //call counterfactual nft contract
 
-            var web3 = new Web3($"https://mainnet.infura.io/v3/53173af3389645d18c3bcac2ee9a751c");
-            var contractABI = "function uri(uint256 id) public returns (string memory)";
+            return metadataLink;
+        }
+
+        public async Task<string?> GetMetadataLink(string? tokenId, string? tokenAddress, string? contractABI, string? functionName)
+        {
+
+            var web3 = new Web3("https://mainnet.infura.io/v3/53173af3389645d18c3bcac2ee9a751c");
             try
             {
-                var contract = web3.Eth.GetContract(contractABI, "0xB25f6D711aEbf954fb0265A3b29F7b9Beba7E55d");
-                var function = contract.GetFunction("uri");
-                object[] parameters = new object[1] { tokenId };
+                var contract = web3.Eth.GetContract(contractABI, tokenAddress);
+                var function = contract.GetFunction(functionName);
+                object[] parameters = new object[1] { tokenId! };
                 var uri = await function.CallAsync<string>(parameters);
+                if (uri == null) return null;
                 return uri.Remove(0, 7); //remove the ipfs portion
             }
             catch (Exception e)
