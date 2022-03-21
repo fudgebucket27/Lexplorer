fcc7a93d34bd7d0a29796dd83c69c6eb05ac6d82
Sebastian Modersohn <im24@msohn.de>
2022-03-21 11:15:11 +0100

    Remove unnecessary ?
    * null check directly above makes this unnecessary

--- Shared/Models/NftMetadata.cs
+++ Shared/Models/NftMetadata.cs
@@ -14,7 +14,7 @@ namespace Lexplorer.Models
             {
                 if (image == null) return null;
                 //remove the ipfs:// when concatenating with mypinata URL
-                return image.StartsWith("ipfs://") ? string.Concat("https://fudgey.mypinata.cloud/ipfs/", image?.Remove(0, 7)) : image;
+                return image.StartsWith("ipfs://") ? string.Concat("https://fudgey.mypinata.cloud/ipfs/", image.Remove(0, 7)) : image;
             }
         }
         public string? name { get; set; }
