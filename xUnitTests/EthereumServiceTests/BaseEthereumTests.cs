4642e8cda1d547415a19ee5245ad2881f43b42ae
John <john.trembath@glencore.com.au>
2022-04-01 13:54:51 +1000

    add search by loopring ens and another unit test

---
+++ xUnitTests/EthereumServiceTests/BaseEthereumTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Lexplorer.Services;
+using Xunit;
+
+namespace xUnitTests.NFTMetaDataTests
+{
+    //shared context for several xUnit test classes
+    //https://xunit.net/docs/shared-context
+    public class EthereumTestsFixture
+    {
+
+        public EthereumService EthS { get; private set; }
+
+        public EthereumTestsFixture()
+        {
+            EthS = new EthereumService();
+        }
+    }
+
+    [CollectionDefinition("EthereumTests collection")]
+    public class EthereumCollection : ICollectionFixture<EthereumTestsFixture>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
+
---
+++ xUnitTests/EthereumServiceTests/TestEthereumService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitTests.NFTMetaDataTests
+{
+    [Collection("EthereumTests collection")]
+    public class TestEthereumService
+    {
+        readonly EthereumTestsFixture fixture;
+
+        public TestEthereumService(EthereumTestsFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Theory]
+        [InlineData("fudgey.loopring.eth")]
+        [InlineData("fudgey.eth")]
+        [InlineData("bubblecum.eth")]
+        public async void TestGetHexAddressFromsEns(string ens)
+        {
+            var hexAddress = await fixture.EthS.GetEthAddressFromEns(ens);
+            Assert.NotNull(hexAddress);
+        }
+    }
+}
