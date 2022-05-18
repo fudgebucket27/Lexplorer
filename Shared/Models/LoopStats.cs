5a769a69398750a687c70d161d68805699a57484
Daniel Soli Stensvik <daniel.soli.stensvik@gmail.com>
2022-05-18 15:32:20 +0200

    Added loopstatsservice, interface and dto.

---
+++ Shared/Models/LoopStats.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lexplorer.Models;
+
+public class LoopStatsDailyCount
+{
+    public long BlockCount { get; set; }
+    public long TransactionCount { get; set; }
+    public long TransferCount { get; set; }
+    public long TransferNFTCount { get; set; }
+    public long TradeNFTCount { get; set; }
+    public long NFTMintCount { get; set; }
+    public long UserCount { get; set; }
+    public long NFTCount { get; set; }
+}
---
+++ Shared/Services/LoopStatsService.cs
@@ -0,0 +1,46 @@
+﻿using Lexplorer.Models;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Diagnostics;
+using System;
+using System.Threading;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using System.Net;
+using Lexplorer.Exceptions;
+
+namespace Lexplorer.Services;
+
+public interface ILoopStatsService
+{
+    Task<LoopStatsDailyCount> GetDailyCount(CancellationToken cancellationToken = default);
+}
+
+public class LoopStatsService : ILoopStatsService
+{
+    private readonly HttpClient _client;
+
+    public LoopStatsService(HttpClient client)
+    {
+        _client = client;
+    }
+
+    public async Task<LoopStatsDailyCount> GetDailyCount(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var dto = await _client.GetFromJsonAsync<LoopStatsDailyCount>("api/GetLastDaysCount", cancellationToken);
+            return dto;
+        }
+        catch (HttpRequestException exc)
+        {
+            if (exc.StatusCode == HttpStatusCode.NotFound)
+            {
+                throw new NotFoundException(nameof(LoopStatsDailyCount));
+            }
+
+            throw;
+        }
+    }
+}
