ff69800973e939b7662c74fde037e00d5e434737
Daniel Soli Stensvik <daniel.soli.stensvik@gmail.com>
2022-05-25 16:06:20 +0200

    Added summary for GetDailyCount

--- Shared/Services/LoopStatsService.cs
+++ Shared/Services/LoopStatsService.cs
@@ -23,6 +23,21 @@ public class LoopStatsService : ILoopStatsService, IDisposable
         _restClient = new RestClient(baseUrl);
     }
 
+    /// <summary>
+    /// Gets the last 24 hours counts, that is utc now and 24 hours in the past. NOT from 00:00 UTC 
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns>
+    /// blockCount
+    /// transactionCount
+    /// transferCount
+    /// transferNFTCount
+    /// tradeNFTCount
+    /// nftMintCount
+    /// userCount
+    /// nftCount
+    /// </returns>
+    /// <exception cref="NotFoundException"></exception>
     public async Task<LoopStatsDailyCount> GetDailyCount(CancellationToken cancellationToken = default)
     {
         try
