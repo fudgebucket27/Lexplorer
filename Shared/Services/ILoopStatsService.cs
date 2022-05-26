f97b0aeefebbf761dfd34fbfdf5781f92a5c189b
Daniel Soli Stensvik <58626043+danielsolistensvik@users.noreply.github.com>
2022-05-26 11:41:25 +0200

    Feature: Add loop stats client and get stats for homepage overview (#149)
    * Added loopstatsservice, interface and dto.
    * Added loopystats url to appsettings
    * Added stats to homepageoverview
    * Added cache for dailystats
    * Now using restsharp client for loopstats, and added nuget extensions.configuration to test and lexplorer projects.
    * Implemented IDisposable
    * Added summary for GetDailyCount

---
+++ Shared/Services/ILoopStatsService.cs
@@ -0,0 +1,11 @@
+﻿using Lexplorer.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lexplorer.Services;
+
+public interface ILoopStatsService
+{
+    void Dispose();
+    Task<LoopStatsDailyCount> GetDailyCount(CancellationToken cancellationToken = default);
+}
