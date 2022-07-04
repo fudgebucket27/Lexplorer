4336b50bfe258236e4fc8c176f3f763f52bfdbdd
Daniel Soli Stensvik <daniel.soli.stensvik@gmail.com>
2022-07-04 19:02:00 +0200

    Added simple tests for method GetWhales

---
+++ xUnitTests/LoopringGraphTests/TestGetWhales.cs
@@ -0,0 +1,31 @@
+﻿using Lexplorer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitTests.LoopringGraphTests;
+
+[Collection("LoopringGraphQL collection")]
+public class TestGetWhales
+{
+    GraphQLTestsFixture fixture;
+    LoopringGraphQLService service;
+
+    public TestGetWhales(GraphQLTestsFixture fixture)
+    {
+        this.fixture = fixture;
+        service = fixture!.LGS;
+    }
+
+    [Fact]
+    public async void GetWhales()
+    {
+        var whales = await service.GetWhales("1", first: 25);
+        Assert.NotEmpty(whales);
+        Assert.Equal(25, whales!.Count);
+        whales.ForEach(x => Assert.NotNull(x.balance));
+    }
+}
