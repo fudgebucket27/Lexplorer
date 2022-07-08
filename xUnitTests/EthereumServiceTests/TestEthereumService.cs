512066c3f00f3aa867a31b895ee6730303f382e2
Sebastian Modersohn <im24@msohn.de>
2022-07-08 20:45:33 +0200

    Fix namespace for Ethereum tests

--- xUnitTests/EthereumServiceTests/TestEthereumService.cs
+++ xUnitTests/EthereumServiceTests/TestEthereumService.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace xUnitTests.NFTMetaDataTests
+namespace xUnitTests.EthereumServiceTests
 {
     [Collection("EthereumTests collection")]
     public class TestEthereumService
